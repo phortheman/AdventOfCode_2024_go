c9df5f9e3728be92b63ec5f7f3e9a8e23f797eda
phortheman <marcuspullo@gmail.com>
2024-12-01 01:02:01 -0500

    feat(day1): added tests

--- solutions/day01/day_01_test.go
+++ solutions/day01/day_01_test.go
@@ -4,8 +4,25 @@ import (
 	"testing"
 )
 
+const input string = `3   4
+4   3
+2   5
+1   3
+3   9
+3   3`
+
 func TestDay1Part1Example(t *testing.T) {
+	expected := 11
+	result, _ := Solver(input)
+	if result != expected {
+		t.Errorf("Expected %d and got %d", expected, result)
+	}
 }
 
 func TestDay1Part2Example(t *testing.T) {
+	expected := 31
+	_, result := Solver(input)
+	if result != expected {
+		t.Errorf("Expected %d and got %d", expected, result)
+	}
 }
